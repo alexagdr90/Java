4b2e905f14dddd23038474ef2e8d19e6312004c1
alexagdr90 <58271425+alexagdr90@users.noreply.github.com>
2021-12-24 10:19:28 +0100

    UpdateMatrices

---
+++ matrices13/src/Matriz_3x3.java
@@ -0,0 +1,17 @@
+public class Matriz_3x3 {
+    public static void main(String[] args) {
+
+        int matriz[][]=new int[3][3];
+
+        //i = filas y j = columnas
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[0].length;j++){
+                matriz[i][j]=(i*matriz.length)+(j+1);
+                System.out.print(matriz[i][j]+" ");
+
+            }
+            System.out.println("");
+        }
+
+    }
+}
