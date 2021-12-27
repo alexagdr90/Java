f0612210d2107847fcb7786535d57104b7d4f97e
alexagdr90 <58271425+alexagdr90@users.noreply.github.com>
2021-12-27 10:10:14 +0100

    UpdateJava

---
+++ arreglos.12/src/StringReemplazar.java
@@ -0,0 +1,8 @@
+public class StringReemplazar {
+    public static void main(String[] args) {
+        String cadena = "Hola Mundo";
+        String cadena2 = cadena.replace( "Mundo" , "Internet" );
+        System.out.println( cadena );
+        System.out.println( cadena2 );
+    }
+}
